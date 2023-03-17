6826207defb56803ea973e0d6bff0ca4d9152d84
Ejembi Patrick Augustine <augustineejembi@gmail.com>
2023-03-17 08:36:38 +0100

    Added_task_14_java_files

---
+++ Task_14/My_major_task.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class My_major_task {public static void main(String[] args) {
+    System.out.println("I am currently currently setting up for my project in testify academy");}
+}
