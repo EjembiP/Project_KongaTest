a07e94c1e944ff03f08c02cf7647b7209a2c1724
Ejembi Patrick Augustine <augustineejembi@gmail.com>
2023-03-24 23:32:29 +0100

    Delete Task_14 directory

--- Task_14/About_me.java
+++
@@ -1,5 +0,0 @@
-package org.example;
-
-public class About_me { public static void main(String[] args) {
-    System.out.println("I am an Idoma by tribe from Benue State Nigeria");}
-}
--- Task_14/My_major_task.java
+++
@@ -1,5 +0,0 @@
-package org.example;
-
-public class My_major_task {public static void main(String[] args) {
-    System.out.println("I am currently currently setting up for my project in testify academy");}
-}
